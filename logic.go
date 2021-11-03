1047a8a6bd092c75e2984e91587ff3e6c80068c7
Mike Papale <mpapale@gmail.com>
2021-11-03 20:49:02 +0000

    Added my battlesnake name

--- logic.go
+++ logic.go
@@ -19,7 +19,7 @@ func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
+		Author:     "mpapale",
 		Color:      "#888888", // TODO: Personalize
 		Head:       "default", // TODO: Personalize
 		Tail:       "default", // TODO: Personalize
