220b9b9df41bf27e882e96c6f1a3469c64fe3953
Mike Papale <mpapale@gmail.com>
2021-11-03 21:25:56 +0000

    Don't hit walls

--- logic.go
+++ logic.go
@@ -43,30 +43,48 @@ func end(state GameState) {
 // where to move -- valid moves are "up", "down", "left", or "right".
 // We've provided some code and comments to get you started.
 func move(state GameState) BattlesnakeMoveResponse {
-	possibleMoves := map[string]bool{
-		"up":    true,
-		"down":  true,
-		"left":  true,
-		"right": true,
+	possibleMoves := struct{
+    up bool
+    down bool
+    left bool
+    right bool
+  }{
+		up:    true,
+		down:  true,
+		left:  true,
+		right: true,
 	}
 
 	// Step 0: Don't let your Battlesnake move back in on it's own neck
 	myHead := state.You.Body[0] // Coordinates of your head
 	myNeck := state.You.Body[1] // Coordinates of body piece directly behind your head (your "neck")
-	if myNeck.X < myHead.X {
-		possibleMoves["left"] = false
-	} else if myNeck.X > myHead.X {
-		possibleMoves["right"] = false
-	} else if myNeck.Y < myHead.Y {
-		possibleMoves["down"] = false
-	} else if myNeck.Y > myHead.Y {
-		possibleMoves["up"] = false
+	switch {
+  case myNeck.X < myHead.X:
+		possibleMoves.left = false
+	case myNeck.X > myHead.X:
+		possibleMoves.right = false
+	case myNeck.Y < myHead.Y:
+		possibleMoves.down = false
+	case myNeck.Y > myHead.Y:
+		possibleMoves.up = false
 	}
 
-	// TODO: Step 1 - Don't hit walls.
-	// Use information in GameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-	// boardWidth := state.Board.Width
-	// boardHeight := state.Board.Height
+  log.Println("myHead: ", myHead.X, myHead.Y)
+  log.Println("board dims: ", state.Board.Width, state.Board.Height)
+	// Step 1 - Don't hit walls.
+  if myHead.X == 0 {
+    possibleMoves.left = false
+  }
+  if myHead.Y == 0 {
+    possibleMoves.down = false
+  }
+  if myHead.X == state.Board.Width-1 {
+    possibleMoves.right = false
+  }
+  if myHead.Y == state.Board.Height-1 {
+    possibleMoves.up = false
+  }
+
 
 	// TODO: Step 2 - Don't hit yourself.
 	// Use information in GameState to prevent your Battlesnake from colliding with itself.
@@ -83,12 +101,20 @@ func move(state GameState) BattlesnakeMoveResponse {
 	var nextMove string
 
 	safeMoves := []string{}
-	for move, isSafe := range possibleMoves {
-		if isSafe {
-			safeMoves = append(safeMoves, move)
-		}
-	}
+  if possibleMoves.up {
+    safeMoves = append(safeMoves, "up")
+  }
+  if possibleMoves.down {
+    safeMoves = append(safeMoves, "down")
+  }
+  if possibleMoves.left {
+    safeMoves = append(safeMoves, "left")
+  }
+  if possibleMoves.right {
+    safeMoves = append(safeMoves, "right")
+  }
 
+  log.Println("safe moves:", safeMoves)
 	if len(safeMoves) == 0 {
 		nextMove = "down"
 		log.Printf("%s MOVE %d: No safe moves detected! Moving %s\n", state.Game.ID, state.Turn, nextMove)
